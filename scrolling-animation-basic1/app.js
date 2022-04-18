f62242f793431535c5e46d20006d0bc8a242e82a
hyuntae <hyuntae.kim@bolttech.io>
2022-04-18 17:40:13 +0900

    Updated basic scroll animation file names

--- scrolling-animation-basic1/app.js
+++ scrolling-animation-basic1/app.js
@@ -1,8 +1,7 @@
-const html = document.documentElement;
 const frameCount = 147;
 
 const currentFrame = index => (
-  `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4,'0')}.jpg`
+  `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
 )
 
 const preloadImages = () => {
@@ -15,16 +14,16 @@ const preloadImages = () => {
 window.onload = function () {
   const canvas = document.getElementById("hero-lightpass");
   const context = canvas.getContext("2d");
-  
-  
+
+
   // Set canvas dimensions
   canvas.width = 1158;
   canvas.height = 770;
-  
+
   const img = new Image()
   img.src = currentFrame(1);
   console.log(img.src);
-  img.onload=() => context.drawImage(img, 0, 0);
+  img.onload = () => context.drawImage(img, 0, 0);
 }
 
 const updateImage = index => {
@@ -37,9 +36,9 @@ const updateImage = index => {
 
 window.addEventListener('scroll', () => {
   const scrollTop = Math.max(document.body.scrollTop, 0);
-  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const maxScrollTop = document.body.scrollHeight - window.innerHeight;
   const scrollFraction = scrollTop / maxScrollTop;
-  const frameIndex = Math.min(frameCount,  Math.ceil(scrollFraction * frameCount) + 1)
+  const frameIndex = Math.min(frameCount, Math.ceil(scrollFraction * frameCount) + 1)
 
   requestAnimationFrame(() => updateImage(frameIndex))
 });
